33a8aa3589428862902b9f97dea33fa1389f4d1f
Inaki Villar <inaki.seri@gmail.com>
2025-10-01 11:27:18 -0700

    adding build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("com.malinskiy.marathon") version "0.10.3-test"
+    id("com.malinskiy.marathon") version "test1"
 }
 
 android {
